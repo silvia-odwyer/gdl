c53be8ab7878166d7648c317d502508f84edb898
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-06-20 22:52:26 +0100

    Separated resizing logic into its own function to maximise code re-use.

--- crate/src/collage.rs
+++ crate/src/collage.rs
@@ -18,17 +18,28 @@ pub fn two_grid(mut photon_img: PhotonImage, mut photon_img2: PhotonImage, width
 
     // distribute the width evenly by allocating the same space to both images
     let img_width = width / 2;
-    let sampling_filter = image::FilterType::Nearest;
-    let image = image::ImageRgba8(image::imageops::resize(&image, img_width, height, sampling_filter));
-    let image2 = image::ImageRgba8(image::imageops::resize(&image2, img_width, height, sampling_filter));
+    let mut imgs = vec![image, image2];
+    resize_imgs(&mut imgs, img_width, height);
 
     let mut container_img : DynamicImage = DynamicImage::new_rgba8(width, height);
 
-    image::imageops::overlay(&mut container_img, &image, 0, 0);
-    image::imageops::overlay(&mut container_img, &image2, image.width(), 0);
+    image::imageops::overlay(&mut container_img, &imgs[0], 0, 0);
+    image::imageops::overlay(&mut container_img, &imgs[1], img_width, 0);
 
     // return the collage
     return PhotonImage {raw_pixels: container_img.raw_pixels(), width: container_img.width(), height: container_img.height()};
+}
+
+/// Resize images in a vec, returns a new vec with resized images.
+fn resize_imgs(mut imgs: &mut Vec<DynamicImage>, img_width: u32, img_height: u32) {
+    let sampling_filter = image::FilterType::Nearest;
+
+    for i in 0..imgs.len() {
+        let item = &imgs[i];
+        let image = image::ImageRgba8(image::imageops::resize(item, img_width, img_height, sampling_filter));
+
+        imgs[i] = image;
+    }
 }
 
 /// Add text to an image.
@@ -62,4 +73,56 @@ pub fn four_grid_text(mut photon_img: PhotonImage, mut photon_img2: PhotonImage,
     
     return photon_img;
 
+}
+
+/// Add text to an image.
+#[wasm_bindgen]
+pub fn four_grid(mut photon_img: PhotonImage, mut photon_img2: PhotonImage, mut photon_img3: PhotonImage, mut photon_img4: PhotonImage, width: u32, height: u32) -> PhotonImage {
+    let mut image = helpers::dyn_image_from_raw(&photon_img);
+    let mut image2 = helpers::dyn_image_from_raw(&photon_img2);
+    let mut image3 = helpers::dyn_image_from_raw(&photon_img3);
+    let mut image4 = helpers::dyn_image_from_raw(&photon_img4);
+
+    // distribute the width evenly by allocating the same space to both images
+    let img_width = width / 2;
+    let img_height = height / 2;
+
+    let sampling_filter = image::FilterType::Nearest;
+    let image = image::ImageRgba8(image::imageops::resize(&image, img_width, img_height, sampling_filter));
+    let image2 = image::ImageRgba8(image::imageops::resize(&image2, img_width, img_height, sampling_filter));
+    let image3 = image::ImageRgba8(image::imageops::resize(&image3, img_width, img_height, sampling_filter));
+    let image4 = image::ImageRgba8(image::imageops::resize(&image4, img_width, img_height, sampling_filter));
+
+    let mut container_img : DynamicImage = DynamicImage::new_rgba8(width, height);
+
+    image::imageops::overlay(&mut container_img, &image, 0, 0);
+    image::imageops::overlay(&mut container_img, &image2, image.width(), 0);
+    image::imageops::overlay(&mut container_img, &image3, 0, image.height());
+    image::imageops::overlay(&mut container_img, &image4, image4.width(), image.height());
+
+    // return the collage
+    let mut photon_img = PhotonImage {raw_pixels: container_img.raw_pixels(), width: container_img.width(), height: container_img.height()};
+    return photon_img;
+}
+
+/// Create a triple grid collage graphic.
+#[wasm_bindgen]
+pub fn triple_grid(mut photon_img: PhotonImage, mut photon_img2: PhotonImage, mut photon_img3: PhotonImage, width: u32, height: u32) -> PhotonImage {
+    let mut image = helpers::dyn_image_from_raw(&photon_img);
+    let mut image2 = helpers::dyn_image_from_raw(&photon_img2);
+    let mut image3 = helpers::dyn_image_from_raw(&photon_img3);
+
+    // distribute the width evenly by allocating the same space to both images
+    let img_width = width / 3;
+    let mut imgs = vec![image, image2, image3];
+    resize_imgs(&mut imgs, img_width, height);
+
+    let mut container_img : DynamicImage = DynamicImage::new_rgba8(width, height);
+
+    image::imageops::overlay(&mut container_img, &imgs[0], 0, 0);
+    image::imageops::overlay(&mut container_img, &imgs[1], img_width, 0);
+    image::imageops::overlay(&mut container_img, &imgs[2], img_width * 2, 0);
+
+    // return the collage
+    return PhotonImage {raw_pixels: container_img.raw_pixels(), width: container_img.width(), height: container_img.height()};
 }
